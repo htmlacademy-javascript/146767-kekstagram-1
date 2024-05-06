677d84f4b1022cc8119e83aa2cf7624913afc1a4
Alexander <web-wild-fox@yandex.ru>
2024-05-06 20:34:11 +0400

    Добваляет функцию для извлечения цифр от 0 до 9

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,23 @@
 // Функции для тренировки
 
 const checkedThePalindrome = function (stringPalindrome) {
+
   const string = stringPalindrome.toLowerCase().replaceAll(' ', '');
   const stringReverse = string.split('').reverse().join('');
+
   return string === stringReverse ? 'true' : 'false';
+
 };
 
 checkedThePalindrome('Лёша на полке клопа нашёл ');
-// console.log(checkedThePalindrome('Лёша на полке клопа нашёл '));
+
+
+const getTheNumbers = function (stringWithNumbers) {
+
+  const numbers = String(stringWithNumbers).match(/\d+/g);
+
+  return numbers ? Number(numbers.join('')) : 'NaN';
+
+};
+
+getTheNumbers('ECMAScript 2022');
