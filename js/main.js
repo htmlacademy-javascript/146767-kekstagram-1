06efd239c2a2ebbd8f6385da54a69b11fd0f69eb
Alexander <web-wild-fox@yandex.ru>
2024-05-15 16:27:04 +0400

    Добавляет функцию для генерации комментариев

--- js/main.js
+++ js/main.js
@@ -4,7 +4,32 @@ const MIN_VALUE = 1;
 const MAX_VALUE = 25;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
-const PHOTO_DESCRIPTIONS = ['Котик', 'Вы', 'Показывают', 'Интересное', 'Продаёте', 'Рыбов'];
+const PHOTO_DESCRIPTIONS = [
+  'Котик',
+  'Вы',
+  'Показывают',
+  'Интересное',
+  'Продаёте',
+  'Рыбов'
+];
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -14,17 +39,19 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-const getObject = () => {
-  const id = getRandomInteger(MIN_VALUE, MAX_VALUE);
-  const url = `photos/${getRandomInteger(MIN_VALUE, MAX_VALUE)}.jpg`;
+const getComment = () => ({
+  id: getRandomInteger(MIN_VALUE, MAX_VALUE),
+  avatar: `avatars/${getRandomInteger(1, 6)}.jpg`,
+  message: getRandomInteger(1, 2) === 1 ? MESSAGES[getRandomInteger(0, MESSAGES.length - 1)] : `${MESSAGES[getRandomInteger(0, MESSAGES.length - 1)]} ${ MESSAGES[getRandomInteger(0, MESSAGES.length - 1)]}`,
+  name: NAMES[getRandomInteger(0, NAMES.length - 1)],
+});
 
-  return {
-    id,
-    url,
-    description: PHOTO_DESCRIPTIONS[getRandomInteger(0, PHOTO_DESCRIPTIONS.length - 1)],
-    likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-  };
-
-};
+const getObject = () => ({
+  id: getRandomInteger(MIN_VALUE, MAX_VALUE),
+  url: `photos/${getRandomInteger(MIN_VALUE, MAX_VALUE)}.jpg`,
+  description: PHOTO_DESCRIPTIONS[getRandomInteger(0, PHOTO_DESCRIPTIONS.length - 1)],
+  likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+  comments: getComment(),
+});
 
 console.log(getObject());
