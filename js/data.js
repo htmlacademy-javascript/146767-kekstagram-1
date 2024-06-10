da8f0513bb496724efc488a5b02c1d9639ff4ecd
Alexander <web-wild-fox@yandex.ru>
2024-06-10 13:28:57 +0400

    Рефакторинг: исправления, именование модуля

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-import {getRandomInteger, getRandomArrElement} from './util.js';
+import {getRandomInteger, getRandomArrElement} from './utils.js';
 
 const PICTURE_COUNT = 25;
 const AVATAR_COUNT = 6;
