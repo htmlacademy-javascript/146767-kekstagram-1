c515f3a2a4887ea1acba92f70c29d600ac085f8e
Alexander <web-wild-fox@yandex.ru>
2024-06-17 12:59:50 +0400

    Добавляет функцию для отрисовки комментариев

--- js/gallery.js
+++ js/gallery.js
@@ -13,7 +13,7 @@ import {
 
 const createComment = (id) => ({
   id,
-  avatar: `avatars/${getRandomInteger(1, AVATAR_COUNT)}.jpg`,
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
   message: getRandomArrElement(MESSAGES),
   name: getRandomArrElement(NAMES),
 });
@@ -65,33 +65,66 @@ const createGalleryData = createGallery();
 
 renderGallery(createGalleryData);
 
+const renderComment = ({avatar, name, message}) => {
+  const commentItem = document.querySelector('.social__comment').cloneNode(true);
+  const commentText = commentItem.querySelector('.social__text');
+  const commentPicture = commentItem.querySelector('.social__picture');
+
+  commentPicture.src = avatar;
+  commentPicture.alt = name;
+  commentText.textContent = message;
+
+  return commentItem;
+};
+
+const renderComments = (dataComments) => {
+  const commentWrapper = document.querySelector('.social__comments');
+  const fragment = document.createDocumentFragment();
+
+  dataComments.forEach((comment) => {
+    fragment.appendChild(renderComment(comment));
+    console.log(fragment);
+  });
+
+  commentWrapper.innerHTML = '';
+  console.log(commentWrapper);
+  commentWrapper.appendChild(fragment);
+};
+
 const renderBigPicture = (evt) => {
   const bigPicture = document.querySelector('.big-picture');
-
   const dataObject = createGalleryData.find(({id}) => id === +evt.target.dataset.id);
-  console.log(dataObject);
 
   bigPicture.querySelector('img').src = dataObject.url;
   bigPicture.querySelector('.likes-count').textContent = dataObject.likes;
   bigPicture.querySelector('.comments-count').textContent = dataObject.comments.length;
   bigPicture.querySelector('.social__caption').textContent = dataObject.description;
+
+  console.log(dataObject);
+  renderComments(dataObject.comments);
 };
 
 const handleClickPicture = (evt) => {
   evt.preventDefault();
 
+  const body = document.querySelector('body');
   const bigPicture = document.querySelector('.big-picture');
+  const commentCount = document.querySelector('.social__comment-count');
+  const commentsLoader = document.querySelector('.comments-loader');
 
   if (evt.target.matches('.picture__img')) {
     console.log(evt.target);
 
     bigPicture.classList.remove('hidden');
+    commentCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    body.classList.add('modal-open');
 
     renderBigPicture(evt);
   }
 
 };
 
-const galleryWrapper = document.querySelector('.pictures');
+const galleryWrapper2 = document.querySelector('.pictures');
 
-galleryWrapper.addEventListener('click', handleClickPicture);
+galleryWrapper2.addEventListener('click', handleClickPicture);
