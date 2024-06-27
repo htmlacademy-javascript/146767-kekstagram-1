0d45ea00843ba8b129f0eb1bb1460e17f7ae786b
Alexander <web-wild-fox@yandex.ru>
2024-06-27 20:42:06 +0400

    Добавляет функцию для отмены закрытия формы при нажатой Esc

--- js/upload-form.js
+++ js/upload-form.js
@@ -3,6 +3,8 @@ import {isEscapeKey} from './utils.js';
 const form = document.querySelector('#upload-select-image');
 const imgUploadForm = form.querySelector('.img-upload__overlay');
 const buttonClose = form.querySelector('.img-upload__cancel');
+const textField = form.querySelector('.text__description');
+const hashtagsField = form.querySelector('.text__hashtags');
 
 const openUploadForm = () => {
   document.body.classList.add('modal-open');
@@ -10,14 +12,20 @@ const openUploadForm = () => {
 
   buttonClose.addEventListener('click', onButtonCloseClick);
   document.addEventListener('keydown', onDocumentKeydown);
+  textField.addEventListener('keydown', onDocumentKeydownCancel);
+  hashtagsField.addEventListener('keydown', onDocumentKeydownCancel);
 };
 
 const closeUploadForm = () => {
   document.body.classList.remove('modal-open');
   imgUploadForm.classList.add('hidden');
 
+  form.reset();
+
   buttonClose.removeEventListener('click', onButtonCloseClick);
   document.removeEventListener('keydown', onDocumentKeydown);
+  textField.removeEventListener('keydown', onDocumentKeydownCancel);
+  hashtagsField.removeEventListener('keydown', onDocumentKeydownCancel);
 };
 
 export const onFileInputChange = () => {
@@ -32,5 +40,11 @@ function onDocumentKeydown(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeUploadForm();
+  }
+}
+
+function onDocumentKeydownCancel(evt) {
+  if (isEscapeKey(evt)) {
+    evt.stopPropagation();
   }
 }
