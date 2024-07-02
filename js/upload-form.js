3bcaf1023e55678b1da7c8d55a5106d664502503
Alexander <web-wild-fox@yandex.ru>
2024-07-02 19:35:47 +0400

    Рефакторинг: исправление замечаний

--- js/upload-form.js
+++ js/upload-form.js
@@ -3,23 +3,24 @@ import {isEscapeKey} from './utils.js';
 const MAX_DESCRIPTION_LENGTH = 140;
 const DESCRIPTION_ERROR_TEXT = 'Комментарий не обязателен. Максимальная длина комментария 140 символов';
 const MAX_TAGS_COUNT = 5;
-const TAGS_COUNT_ERROR_TEXT = 'Нельзя указывать больше пяти хэш-тегов';
-const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
-const VALID_SYMBOLS_ERROR_TEXT = `Обратите внимание, что хэш-теги необязательны!
+const TAG_PATTERN = /^#[a-zа-яё0-9]{1,19}$/i;
+const TAGS_ERROR_TEXT = `Обратите внимание, что хэш-теги необязательны!
   Хеш-тег должен начинаться с символа #,
   не может состоять только из одной решётки
   и должен содержать только латинские буквы и цифры.
   Хэш-теги должны разделяться пробелами.
-  Максимальная длина одного хэш-тега 20 символов, включая решётку.`;
-const UNIQUE_TAGS_ERROR_TEXT = 'Есть повторяющиеся теги';
+  Максимальная длина одного хэш-тега 20 символов, включая решётку.
+  Нет повторяющихся тегов.
+  Нельзя указывать больше пяти хэш-тегов`;
 
 const form = document.querySelector('#upload-select-image');
 const imgUploadForm = form.querySelector('.img-upload__overlay');
 const buttonClose = form.querySelector('.img-upload__cancel');
 const descriptionField = form.querySelector('.text__description');
 const hashtagsField = form.querySelector('.text__hashtags');
+const imgUploadButton = form.querySelector('.img-upload__input');
 
-const pristine = new Pristine (form, {
+const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__error-text',
@@ -31,8 +32,6 @@ const openUploadForm = () => {
 
   buttonClose.addEventListener('click', onButtonCloseClick);
   document.addEventListener('keydown', onDocumentKeydown);
-  hashtagsField.addEventListener('keydown', onDocumentKeydownCancel);
-  descriptionField.addEventListener('keydown', onDocumentKeydownCancel);
 };
 
 const closeUploadForm = () => {
@@ -44,28 +43,26 @@ const closeUploadForm = () => {
 
   buttonClose.removeEventListener('click', onButtonCloseClick);
   document.removeEventListener('keydown', onDocumentKeydown);
-  hashtagsField.removeEventListener('keydown', onDocumentKeydownCancel);
-  descriptionField.removeEventListener('keydown', onDocumentKeydownCancel);
 };
 
-export const onFileInputChange = () => {
+const onFileInputChange = () => {
   openUploadForm();
 };
+
+imgUploadButton.addEventListener('change', onFileInputChange);
 
 function onButtonCloseClick() {
   closeUploadForm();
 }
 
+const isFildeFocused = () =>
+  document.activeElement === hashtagsField ||
+  document.activeElement === descriptionField;
+
 function onDocumentKeydown(evt) {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isFildeFocused()) {
     evt.preventDefault();
     closeUploadForm();
-  }
-}
-
-function onDocumentKeydownCancel(evt) {
-  if (isEscapeKey(evt)) {
-    evt.stopPropagation();
   }
 }
 
@@ -74,53 +71,41 @@ form.addEventListener('submit', (evt) => {
   pristine.validate();
 });
 
-const isValidDescription = (description) => description.length <= MAX_DESCRIPTION_LENGTH;
+const isDescriptionValid = (description) => description.length <= MAX_DESCRIPTION_LENGTH;
 
-const getTagsArr = (tags) => tags.trim().split(' ').filter((tag) => tag.trim().length);
+const normalizeTags = (tags) => tags.trim().split(' ').filter((tag) => tag.trim().length);
 
-const isValidTags = (tags) => {
-  const tagsArr = getTagsArr(tags);
+const isSymbolsValid = (tags) => {
 
-  if (tagsArr.length === 0) {
+  if (tags.length === 0) {
     return true;
   }
 
-  return tagsArr.every((tag) => VALID_SYMBOLS.test(tag));
+  return tags.every((tag) => TAG_PATTERN.test(tag));
 };
 
-const isValidCount = (tags) => {
-  const tagsArr = getTagsArr(tags);
+const isTagsCountValid = (tags) => tags.length <= MAX_TAGS_COUNT;
 
-  return tagsArr.length <= MAX_TAGS_COUNT;
-};
-
-const isUniqueTags = (tags) => {
-  const tagsArr = getTagsArr(tags);
-  const lowerCaseArr = tagsArr.map((tag) => tag.toLowerCase());
+const isTagsUnique = (tags) => {
+  const lowerCaseArr = tags.map((tag) => tag.toLowerCase());
 
   return lowerCaseArr.length === new Set(lowerCaseArr).size;
 };
 
+const isValidTags = (tags) => {
+  const tagsArr = normalizeTags(tags);
+
+  return isSymbolsValid(tagsArr) && isTagsCountValid(tagsArr) && isTagsUnique(tagsArr);
+};
+
 pristine.addValidator(
   descriptionField,
-  isValidDescription,
+  isDescriptionValid,
   DESCRIPTION_ERROR_TEXT
 );
 
 pristine.addValidator(
   hashtagsField,
   isValidTags,
-  VALID_SYMBOLS_ERROR_TEXT
-);
-
-pristine.addValidator(
-  hashtagsField,
-  isValidCount,
-  TAGS_COUNT_ERROR_TEXT
-);
-
-pristine.addValidator(
-  hashtagsField,
-  isUniqueTags,
-  UNIQUE_TAGS_ERROR_TEXT
+  TAGS_ERROR_TEXT
 );
