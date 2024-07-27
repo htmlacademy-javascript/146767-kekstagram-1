3e589d5709a7caf5b9c7db62797f9c1b602dc29e
Alexander <web-wild-fox@yandex.ru>
2024-07-27 21:04:35 +0400

    Изменяет подключение модулей в точке входа

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
-import './gallery.js';
+import {renderGallery} from './gallery.js';
+import {getData} from './upload-send-data.js';
 import './upload-form.js';
+
+getData()
+  .then((photos) => {
+    renderGallery(photos);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
