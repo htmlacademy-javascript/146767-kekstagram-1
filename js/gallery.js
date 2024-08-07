6c55b55e766a60ee96e245a1cc23966bb5b6d746
Alexander <web-wild-fox@yandex.ru>
2024-08-07 16:23:42 +0400

    Добавляет функции для фильтрации галереи

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,14 @@
+import {debounce} from './utils.js';
 import {openBigPicture} from './big-picture.js';
+
+const FILTER_UPDATE_TIME = 500;
 
 let photosDataArr = [];
 
 const pictureTemplate = document.querySelector('#picture').content;
 const gallery = document.querySelector('.pictures');
+const imgFilters = document.querySelector('.img-filters');
+const imgFiltersForm = document.querySelector('.img-filters__form');
 
 const createPictureEl = ({id, url, likes, comments}) => {
   const picture = pictureTemplate.cloneNode(true);
@@ -30,8 +35,32 @@ const renderGallery = (photos) => {
 
 };
 
+const onFilterClick = (evt) => {
+  const filter = evt.target.id;
+  const photosFilterArr = photosDataArr.slice();
+
+  gallery
+    .querySelectorAll('.picture')
+    .forEach((picture) => picture.remove());
+
+  switch (filter) {
+    case 'filter-default':
+      renderGallery(photosFilterArr);
+      break;
+    case 'filter-random':
+      renderGallery(photosFilterArr.sort(() => Math.random() - 0.5).slice(0, 10));
+      break;
+    case 'filter-discussed':
+      renderGallery(photosFilterArr.sort((a, b) => b.comments.length - a.comments.length));
+      break;
+  }
+};
+
 export const initGallery = (photos) => {
   photosDataArr = photos;
+
+  imgFilters.classList.remove('img-filters--inactive');
+  imgFiltersForm.addEventListener('click', debounce(onFilterClick, FILTER_UPDATE_TIME));
 
   renderGallery(photosDataArr);
 };
